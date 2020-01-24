e1655d936cda8c749b480e4b7ea32a74fdc048ca
Sean Bethel <tech@americanmadeweb.com>
2020-01-24 14:50:51 -0500

    add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION=$(git rev-parse --short HEAD)
+
+if [ ! -d "../auth_proxy_builds" ]; then
+    mkdir "../auth_proxy_builds"
+fi
+
+FILENAME="powerbi_auth_proxy_build_$VERSION"
+
+if [ ! -f "../auth_proxy_builds/$FILENAME.zip" ]; then
+
+    composer archive -f zip --dir="../auth_proxy_builds" --file="$FILENAME" --ignore-filters
+
+fi
