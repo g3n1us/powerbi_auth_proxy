403bae6c3b316e2a6f55a8395850d30a2536df0d
Sean Bethel <tech@americanmadeweb.com>
2020-01-24 15:18:36 -0500

    update build script

--- build.sh
+++ build.sh
@@ -6,8 +6,23 @@ if [ ! -d "../auth_proxy_builds" ]; then
     mkdir "../auth_proxy_builds"
 fi
 
-FILENAME="powerbi_auth_proxy_build_$VERSION"
+if [ ! -d "../auth_proxy_builds/versions" ]; then
+    mkdir "../auth_proxy_builds/versions"
+fi
 
-if [ ! -f "../auth_proxy_builds/$FILENAME.zip" ]; then
+FILENAME="powerbi_auth_proxy_build_$VERSION"
+FILEPATH="../auth_proxy_builds/$FILENAME.zip"
+
+TIMESTAMP=$(date +%F--%H-%M-%S)
+VERSIONSDIR="../auth_proxy_builds/versions"
+
+if [ ! -f "$FILEPATH" ]; then
     composer archive -f zip --dir="../auth_proxy_builds" --file="$FILENAME" --ignore-filters
+    cd $VERSIONSDIR
+    ln -s "../$FILENAME.zip" "build_$TIMESTAMP.zip"
+    ln -s "../$FILENAME.zip" "current.zip"
+    echo $VERSION > hash.txt
+
+else
+    echo "build is current"
 fi
