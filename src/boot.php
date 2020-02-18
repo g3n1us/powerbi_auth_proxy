94447d13443e52d35c831b0cd9f59c1789d79907
Sean Bethel <tech@americanmadeweb.com>
2020-02-18 10:24:37 -0500

    change config dir to root

--- src/boot.php
+++ src/boot.php
@@ -4,8 +4,9 @@ namespace BlueRaster\PowerBIAuthProxy;
 require(__DIR__ . '/helpers.php');
 
 if(!session_status()) session_start();
-// 	$dotenv_dir = dirname(getcwd());
-$dotenv_dir = dirname($_SERVER['DOCUMENT_ROOT']);
+
+// $dotenv_dir = dirname($_SERVER['DOCUMENT_ROOT']);
+$dotenv_dir = dirname(__DIR__);
 
 if(file_exists("$dotenv_dir/.env")){
 	$dotenv = \Dotenv\Dotenv::create($dotenv_dir);
