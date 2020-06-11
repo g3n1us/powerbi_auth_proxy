73882ed6c498621833bde68f656d018b27b35bda
Sean Bethel <tech@americanmadeweb.com>
2020-06-11 16:47:07 -0400

    add build script

---
+++ src/assets/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GIT_CLEAN=$(git status -s)
+
+if [ ! -e "$GIT_CLEAN" ]; then
+	echo "There are changed files that must be committed before proceeding."
+	echo "exiting."
+	exit
+	
+fi
+
+npm run production
+git commit -am 'automated build.'
+cd ../.. 
+bash build.sh
